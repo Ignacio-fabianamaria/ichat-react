a3ce30156bb637df80a0a1a5e39acabe9aaadaf5
Siigmaf <faby.tbe@gmail.com>
2023-10-25 09:48:49 -0300

    style:initial configs tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      sans: ["Open Sans", "sans-serif"]
+    },
+    extend: {
+      colors:{
+        'blue':'#023AFF',
+        'blue-100':'#03009206',
+        'blue-200':'#023AFF08',
+        'blue-300':'#023Aff15',
+        'green':'#5CDE88',
+        'gray-100':'#E1E1E1',
+        'gray-200':'#999999',
+        'gray-300':'#666666',
+        'black':'#333333'
+      },
+      borderRadius:{
+        rounded:{
+          100:'10px',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
